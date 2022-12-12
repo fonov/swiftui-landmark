8765a6c2d17d4bc6324ae567cb9fda0aecbb7683
Sergei Fonov <sergeufonov@gmail.com>
2022-12-12 18:58:47 +0400

    setup watchOS app

--- landmark/landmarkApp.swift
+++ landmark/landmarkApp.swift
@@ -16,5 +16,9 @@ struct LandmarkApp: App {
       ContentView()
         .environmentObject(modelData)
     }
+
+    #if os(watchOS)
+    WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+    #endif
   }
 }
